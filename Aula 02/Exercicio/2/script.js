940e58542618cc232a2d5b4febfe2434c2aed11e
Enzo <enzodeveloper@hotmail.com>
2023-07-16 18:40:59 -0300

    first commit

---
+++ Aula 02/Exercicio/2/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    let texto = document.getElementById("texto").value;
+
+    if (texto.length <= 20) {
+        document.getElementById('resposta').innerHTML = 'O texto é pequeno.';
+    } else if (texto.length <= 30) {
+        document.getElementById('resposta').innerHTML = 'O texto é médio.';
+    } else if (texto.length <= 40) {
+        document.getElementById('resposta').innerHTML = 'O texto é grande.';
+    } else {
+        document.getElementById('resposta').innerHTML = 'Não vou contar o tamanho da frase.';
+    };
+};
