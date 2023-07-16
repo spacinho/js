69c217b8e40008c06193d53aed1df0d99bcac060
Enzo <enzodeveloper@hotmail.com>
2023-07-16 19:57:50 -0300

    first commit

--- Aula 02/Prova/script.js
+++ Aula 02/Prova/script.js
@@ -1,10 +1,11 @@
-function myFunction() {
-    let numero = Number(document.getElementById("numero").value);
-    if (numero < 18) {
-        alert("Você é menor de idade.");
-    } else if (numero == 18) {
-        alert("Você acabou de atingir a maioridade.")
-    } else if (numero > 18) {
-        alert("Você é maior de idade")
-    };
+let idade = prompt('Digite a sua idade');
+
+if (idade.length == 0) {
+    alert("Digite uma idade válida");
+} else if (idade < 18) {
+    alert("Você é menor de idade.");
+} else if (idade == 18) {
+    alert("Você acabou de atingir a maioridade.")
+} else if (idade > 18) {
+    alert("Você é maior de idade")
 };
